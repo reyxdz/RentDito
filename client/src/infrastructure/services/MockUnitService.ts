c304c0e5c09b25797d429582cb435f17d8967d52
userMarcPaul <rjsanoy08@gmail.com>
2026-04-08 02:01:22 +0800

    rooms  3 & 4

--- client/src/infrastructure/services/MockUnitService.ts
+++ client/src/infrastructure/services/MockUnitService.ts
@@ -1,5 +1,6 @@
 import type { Unit } from '../../domain/entities/Unit';
-
+// White dorm unit images ========================================================
+// Room 2
 import rm2Img1 from '../../assets/properties/white_dorm/units/room2/room2_img1.jpg';
 import rm2Img2 from '../../assets/properties/white_dorm/units/room2/room2_img2.jpg';
 import rm2Img3 from '../../assets/properties/white_dorm/units/room2/room2_img3.jpg';
@@ -7,6 +8,19 @@ import rm2Img4 from '../../assets/properties/white_dorm/units/room2/room2_img4.j
 import rm2Img5 from '../../assets/properties/white_dorm/units/room2/room2_img5.jpg';
 import rm2Img6 from '../../assets/properties/white_dorm/units/room2/room2_img6.jpg';
 import rm2Img7 from '../../assets/properties/white_dorm/units/room2/room2_img7.jpg';
+// Room 3 
+import rm3Img1 from '../../assets/properties/white_dorm/units/room3/room3_img1.jpg';
+import rm3Img2 from '../../assets/properties/white_dorm/units/room3/room3_img2.jpg';
+import rm3Img3 from '../../assets/properties/white_dorm/units/room3/room3_img3.jpg';
+import rm3Img4 from '../../assets/properties/white_dorm/units/room3/room3_img4.jpg';
+// Room 4
+import rm4Img1 from '../../assets/properties/white_dorm/units/room4/room4_img1.jpg';
+import rm4Img2 from '../../assets/properties/white_dorm/units/room4/room4_img2.jpg';
+import rm4Img3 from '../../assets/properties/white_dorm/units/room4/room4_img3.jpg';
+
+//==============================================================================
+
+
 
 // =============================================================================
 // HARDCODED UNIT DATA
@@ -34,6 +48,43 @@ const MOCK_UNITS: Unit[] = [
       'Vacant as a room or for Bedspace',
     ],
   },
+    {
+    id: 'unit-white-dorm-room3',
+    propertyId: 'prop-white-dorm',
+    name: 'Room 3',
+    accommodationType: 'Bedspace',
+    images: [
+      rm3Img1, rm3Img2, rm3Img3, rm3Img4
+    ],
+    monthlyRent: 2500,
+    capacity: 4,
+    currentOccupants: 0,
+    vacancies: 4,
+    status: 'Available',
+    features: ['Foam', 'Pillow', 'WiFi', 'Clip Fan', 'Table', 'Chair'],
+    otherDetails: [
+      'Vacant as a room or for Bedspace',
+    ],
+  },
+      {
+    id: 'unit-white-dorm-room4',
+    propertyId: 'prop-white-dorm',
+    name: 'Room 4',
+    accommodationType: 'Bedspace',
+    images: [
+      rm4Img1, rm4Img2, rm4Img3
+    ],
+    monthlyRent: 2500,
+    capacity: 2,
+    currentOccupants: 0,
+    vacancies: 2,
+    status: 'Available',
+    features: ['Foam', 'Pillow', 'WiFi', 'Clip Fan', 'Table', 'Chair'],
+    otherDetails: [
+      'Vacant as a room or for Bedspace',
+    ],
+  },
+
 
 ];
 
