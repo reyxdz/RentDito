b7a1c6f2a72a20ad98e5ef1ad3ffd9a33c3a73ce
userMarcPaul <rjsanoy08@gmail.com>
2026-04-08 23:46:58 +0800

    Adding room 15 & 16 data

--- client/src/infrastructure/services/MockUnitService.ts
+++ client/src/infrastructure/services/MockUnitService.ts
@@ -17,18 +17,28 @@ import rm3Img4 from '../../assets/properties/white_dorm/units/room3/room3_img4.j
 import rm4Img1 from '../../assets/properties/white_dorm/units/room4/room4_img1.jpg';
 import rm4Img2 from '../../assets/properties/white_dorm/units/room4/room4_img2.jpg';
 import rm4Img3 from '../../assets/properties/white_dorm/units/room4/room4_img3.jpg';
-
 // Room 6
 import rm6Img1 from '../../assets/properties/white_dorm/units/room6/room6_img1.jpg';
 import rm6Img2 from '../../assets/properties/white_dorm/units/room6/room6_img2.jpg';
 import rm6Img3 from '../../assets/properties/white_dorm/units/room6/room6_img3.jpg';
-
 // Room 7
 import rm7Img1 from '../../assets/properties/white_dorm/units/room7/room7_img1.jpg';
 import rm7Img2 from '../../assets/properties/white_dorm/units/room7/room7_img2.jpg';
 import rm7Img3 from '../../assets/properties/white_dorm/units/room7/room7_img3.jpg';
 import rm7Img4 from '../../assets/properties/white_dorm/units/room7/room7_img4.jpg';
 import rm7Img5 from '../../assets/properties/white_dorm/units/room7/room7_img5.jpg';
+// Room 15
+import rm15Img1 from '../../assets/properties/white_dorm/units/room15/room15_img1.jpg';
+import rm15Img2 from '../../assets/properties/white_dorm/units/room15/room15_img2.jpg';
+import rm15Img3 from '../../assets/properties/white_dorm/units/room15/room15_img3.jpg';
+import rm15Img4 from '../../assets/properties/white_dorm/units/room15/room15_img4.jpg';
+import rm15Img5 from '../../assets/properties/white_dorm/units/room15/room15_img5.jpg';
+// Room 16
+import rm16Img1 from '../../assets/properties/white_dorm/units/room16/room16_img1.jpg';
+import rm16Img2 from '../../assets/properties/white_dorm/units/room16/room16_img2.jpg';
+import rm16Img3 from '../../assets/properties/white_dorm/units/room16/room16_img3.jpg';
+import rm16Img4 from '../../assets/properties/white_dorm/units/room16/room16_img4.jpg';
+import rm16Img5 from '../../assets/properties/white_dorm/units/room16/room16_img5.jpg';
 //==============================================================================
 
 
@@ -116,21 +126,58 @@ const MOCK_UNITS: Unit[] = [
     {
     id: 'unit-white-dorm-room7',
     propertyId: 'prop-white-dorm',
-    name: 'Room 2',
+    name: 'Room 7',
     accommodationType: 'Bedspace',
     images: [
       rm7Img1, rm7Img2, rm7Img3, rm7Img4, rm7Img5
     ],
     monthlyRent: 2500,
-    capacity: 8,
+    capacity: 4,
     currentOccupants: 0,
-    vacancies: 8,
+    vacancies: 4,
     status: 'Available',
     features: ['Foam', 'Pillow', 'WiFi', 'Clip Fan', 'Table', 'Chair'],
     otherDetails: [
       'Vacant as a room or for Bedspace',
     ],
   },
+    {
+    id: 'unit-white-dorm-room15',
+    propertyId: 'prop-white-dorm',
+    name: 'Room 15',
+    accommodationType: 'Bedspace',
+    images: [
+      rm15Img1, rm15Img2, rm15Img3, rm15Img4, rm15Img5
+    ],
+    monthlyRent: 2500,
+    capacity: 4,
+    currentOccupants: 0,
+    vacancies: 4,
+    status: 'Available',
+    features: ['Foam', 'Pillow', 'WiFi', 'Clip Fan', 'Table', 'Chair'],
+    otherDetails: [
+      'Vacant as a room or for Bedspace',
+    ],
+  },
+    {
+    id: 'unit-white-dorm-room16',
+    propertyId: 'prop-white-dorm',
+    name: 'Room 16',
+    accommodationType: 'Bedspace',
+    images: [
+      rm16Img1, rm16Img2, rm16Img3, rm16Img4, rm16Img5
+    ],
+    monthlyRent: 2500,
+    capacity: 4,
+    currentOccupants: 0,
+    vacancies: 4,
+    status: 'Available',
+    features: ['Foam', 'Pillow', 'WiFi', 'Clip Fan', 'Table', 'Chair'],
+    otherDetails: [
+      'Vacant as a room or for Bedspace',
+    ],
+  },
+
 
 ];
 
